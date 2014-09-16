ed34316fcfdb3e18172dce909a4b70636a1334b7
aksoft <718541683@qq.com>
2014-09-16 15:48:21 +0800

    表单分页ip

--- app/helpers/admins_helper.rb
+++ app/helpers/admins_helper.rb
@@ -1,2 +1,17 @@
 module AdminsHelper
+  require 'open-uri'
+  require 'json'
+  def hello_world(name)
+    "hello #{name}"
+  end
+  def get_location
+    ip = request.remote_ip
+    url = "http://ip.taobao.com/service/getIpInfo.php?ip=" + ip
+    response = Net::HTTP.get_response(URI.parse(url)).body
+    response = JSON.parse(response)['data']
+  end
+
+  def get_country
+    response = get_location['country']
+  end
 end
